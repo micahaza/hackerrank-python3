f4a2c791529bcd5fedc1c78c39dc7fa248c75b7a
Pető Zoltán <bajgli@gmail.com>
2019-09-05 22:22:42 +0200

    python print

---
+++ solutions/python_print.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-print/problem
+
+
+def print_demo():
+    n = int(input())
+    for x in range(1, n + 1):
+        print(x, end='')
---
+++ tests/test_python_print.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+from solutions.python_print import print_demo
+import pytest
+import sys
+import io
+
+
+test_data = [
+    (['3'], '123'),
+    (['1'], '1'),
+    (['10'], '12345678910')
+]
+
+
+@pytest.mark.parametrize("test_input, expected_output", test_data)
+def test_if_else(test_input, expected_output):
+    with patch('builtins.input', side_effect=test_input):
+        stdout = sys.stdout
+        sys.stdout = io.StringIO()
+        print_demo()
+        output = sys.stdout.getvalue().strip()
+        print(output)
+        sys.stdout = stdout
+        assert output == expected_output
